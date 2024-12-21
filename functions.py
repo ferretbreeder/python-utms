6d7347d0cdcd3c0d9ed43713e8ce881f7c73d771
Silver <ferretbreeder@gmail.com>
2024-12-21 13:12:00 -0500

    jesus but I think I finally reconciled my terrible merge

--- functions.py
+++ functions.py
@@ -26,32 +26,50 @@ def content_grabber(html):
         if 'indiana.edu' in link['href'] or 'iu.edu' in link['href']:
             if "mailto:" not in link['href'] and ".png" not in link['href'] and "tel:" not in link['href'] and ".jpg" not in link['href'] and 'https://one.iu.edu' not in link['href'] and "machform" not in link['href']:
                 if len(link.contents) == 1:
-                    dirty_content = str(link.contents[0]).replace(" ", "-").lower().strip("\'")
-                    char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
-                    hypen_content = char_regex.sub('', dirty_content)
-                    clean_content = hypen_content.replace('--', '-')
-                    link_content_list.append(clean_content)
+                    if '#' in link['href']:
+                        url = link['href'].split('#')[0]
+                        anchor = '#' + link['href'].split('#')[1]
+                        dirty_content = str(link.contents[0]).replace(" ", "-").lower().strip("\'")
+                        char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
+                        hypen_content = char_regex.sub('', dirty_content)
+                        clean_content = hypen_content.replace('--', '-') + anchor
+                        link_content_list.append(clean_content)
+                    else:
+                        dirty_content = str(link.contents[0]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'")
+                        char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
+                        hypen_content = char_regex.sub('', dirty_content)
+                        clean_content = hypen_content.replace('--', '-')
+                        link_content_list.append(clean_content)
                 else:
-                    dirty_content = str(link.contents[2]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'") + "-button"
-                    char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
-                    hypen_content = char_regex.sub('', dirty_content)
-                    clean_content = hypen_content.replace('--', '-')
-                    link_content_list.append(clean_content)
-            else:
-                if '#' in link['href']:
-                    url = link['href'].split('#')[0]
-                    anchor = '#' + link['href'].split('#')[1]
-                    dirty_content = str(link.contents[2]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'") + "-button"
-                    char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
-                    hypen_content = char_regex.sub('', dirty_content)
-                    clean_content = hypen_content.replace('--', '-') + anchor
-                    link_content_list.append(clean_content)
-                else:
-                    dirty_content = str(link.contents[2]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'") + "-button"
-                    char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
-                    hypen_content = char_regex.sub('', dirty_content)
-                    clean_content = hypen_content.replace('--', '-')
-                    link_content_list.append(clean_content)
+                    if '#' in link['href']:
+                        url = link['href'].split('#')[0]
+                        anchor = '#' + link['href'].split('#')[1]
+                        dirty_content = str(link.contents[2]).replace(" ", "-").lower().strip("\'") + "-button"
+                        char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
+                        hypen_content = char_regex.sub('', dirty_content)
+                        clean_content = hypen_content.replace('--', '-') + anchor
+                        link_content_list.append(clean_content)
+                    else:
+                        dirty_content = str(link.contents[2]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'") + "-button"
+                        char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
+                        hypen_content = char_regex.sub('', dirty_content)
+                        clean_content = hypen_content.replace('--', '-')
+                        link_content_list.append(clean_content)
+                # if '#' in link['href']:
+                #     url = link['href'].split('#')[0]
+                #     anchor = '#' + link['href'].split('#')[1]
+                #     dirty_content = str(link.contents[2]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'") + "-button"
+                #     char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
+                #     hypen_content = char_regex.sub('', dirty_content)
+                #     clean_content = hypen_content.replace('--', '-') + anchor
+                #     link_content_list.append(clean_content)
+                # else:
+                #     print(link.contents)
+                #     dirty_content = str(link.contents[0]).strip().replace(" ", "-").lower().strip().strip("\n").strip("\'") + "-button"
+                #     char_regex = re.compile(r'\s*[^a-zA-Z0-9\-]\s*')
+                #     hypen_content = char_regex.sub('', dirty_content)
+                #     clean_content = hypen_content.replace('--', '-')
+                #     link_content_list.append(clean_content)
         else:
             pass
 
